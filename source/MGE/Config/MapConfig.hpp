36ee66a6e806b20315ba4d785168eecc872d1cf5
juliencombattelli <julien.combattelli@hotmail.com>
2016-09-01 20:30:01 -0400

    Add MapConfig file. It lets you choose the Map class used by the MGE.

---
+++ source/MGE/Config/MapConfig.hpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : MapConfig.hpp
+// Author      : Julien Combattelli
+// EMail       : julien.combattelli@hotmail.com
+// Date		   : Sep 1, 2016
+// Version     : 1.0.0
+// Copyright   : This file is part of MortalisGameEngine project which is released under
+//               MIT license. See file LICENSE.txt for full license details
+// Description : 
+//============================================================================
+
+#ifndef MAPCONFIG_HPP_
+#define MAPCONFIG_HPP_
+
+// If needed, replace this include with yours
+#include <MUL/SFE/TileMap.hpp>
+
+namespace mge
+{
+
+// If needed, replace with your custom map class
+using Map = mul::sfe::TileMap;
+
+} // namespace mge
+
+#endif // MAPCONFIG_HPP_
