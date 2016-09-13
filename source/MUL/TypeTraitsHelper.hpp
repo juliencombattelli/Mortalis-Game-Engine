9841149a8bced93641f3aa60e2542311ed30a38b
juliencombattelli <julien.combattelli@hotmail.com>
2016-09-12 20:43:44 -0400

    Remove mul namespace generation in TypeTraitsHelper macros.

--- source/MUL/TypeTraitsHelper.hpp
+++ source/MUL/TypeTraitsHelper.hpp
@@ -18,32 +18,29 @@
  * 		cout << has_reserve<vector<int>, void(int)>::value;
  */
 #define MUL_DECLARE_HAS_METHOD_TRAITS(TYPE_TRAIT_NAME, METHOD_NAME)						\
-namespace mul																			\
-{																						\
-	template<typename CLASS, typename SIGNATURE = void()>								\
-	struct TYPE_TRAIT_NAME {};															\
-																						\
-	template<typename T,typename Ret, typename... Args>									\
-	struct TYPE_TRAIT_NAME<T, Ret(Args...)>												\
-	{																					\
-	private:																			\
-																						\
-		template<typename U>															\
-		static constexpr auto check(U*) 												\
-			-> typename std::is_same													\
-				<																		\
-					decltype(std::declval<U>().METHOD_NAME( std::declval<Args>()... )),	\
-					Ret																	\
-				>::type;																\
-																						\
-		template<typename>																\
-		static constexpr std::false_type check(...);									\
-																						\
-	public:																				\
-																						\
-		static constexpr bool value = decltype(check<T>(0))::value;						\
-	};																					\
-} // namespace mul
+template<typename CLASS, typename SIGNATURE = void()>								\
+struct TYPE_TRAIT_NAME {};															\
+																					\
+template<typename T,typename Ret, typename... Args>									\
+struct TYPE_TRAIT_NAME<T, Ret(Args...)>												\
+{																					\
+private:																			\
+																					\
+	template<typename U>															\
+	static constexpr auto check(U*) 												\
+		-> typename std::is_same													\
+			<																		\
+				decltype(std::declval<U>().METHOD_NAME( std::declval<Args>()... )),	\
+				Ret																	\
+			>::type;																\
+																					\
+	template<typename>																\
+	static constexpr std::false_type check(...);									\
+																					\
+public:																				\
+																					\
+	static constexpr bool value = decltype(check<T>(0))::value;						\
+}
 
 /*
  * 	Example :
@@ -51,47 +48,44 @@ namespace mul																			\
  * 		cout << has_reserve<vector<int>, int>::value;
  */
 #define MUL_DECLARE_HAS_METHOD_TRAITS_DISCARD_RETURN_TYPE(TYPE_TRAIT_NAME, METHOD_NAME)	\
-namespace mul																			\
-{																						\
-	template<typename CLASS, typename SIGNATURE = void()>								\
-	struct TYPE_TRAIT_NAME {};															\
-																						\
-	template<typename T, typename... Args>												\
-	struct TYPE_TRAIT_NAME<T, Args...>													\
-	{																					\
-	private:																			\
-																						\
-		template<typename U>															\
-		static constexpr auto check(U*) 												\
-			-> decltype(std::declval<U>().METHOD_NAME( std::declval<Args>()... ),		\
-						std::true_type());												\
-																						\
-		template<typename>																\
-		static constexpr std::false_type check(...);									\
-																						\
-	public:																				\
-																						\
-		static constexpr bool value = decltype(check<T>(0))::value;						\
-	};																					\
-																						\
-	template<typename T, typename Ret, typename... Args>								\
-	struct TYPE_TRAIT_NAME<T, Ret(Args...)>												\
-	{																					\
-	private:																			\
-																						\
-		template<typename U>															\
-		static constexpr auto check(U*) 												\
-			-> decltype(std::declval<U>().METHOD_NAME( std::declval<Args>()... ),		\
-						std::true_type());												\
-																						\
-		template<typename>																\
-		static constexpr std::false_type check(...);									\
-																						\
-	public:																				\
-																						\
-		static constexpr bool value = decltype(check<T>(0))::value;						\
-	};																					\
-} // namespace mul
+template<typename CLASS, typename SIGNATURE = void()>								\
+struct TYPE_TRAIT_NAME {};															\
+																					\
+template<typename T, typename... Args>												\
+struct TYPE_TRAIT_NAME<T, Args...>													\
+{																					\
+private:																			\
+																					\
+	template<typename U>															\
+	static constexpr auto check(U*) 												\
+		-> decltype(std::declval<U>().METHOD_NAME( std::declval<Args>()... ),		\
+					std::true_type());												\
+																					\
+	template<typename>																\
+	static constexpr std::false_type check(...);									\
+																					\
+public:																				\
+																					\
+	static constexpr bool value = decltype(check<T>(0))::value;						\
+};																					\
+																					\
+template<typename T, typename Ret, typename... Args>								\
+struct TYPE_TRAIT_NAME<T, Ret(Args...)>												\
+{																					\
+private:																			\
+																					\
+	template<typename U>															\
+	static constexpr auto check(U*) 												\
+		-> decltype(std::declval<U>().METHOD_NAME( std::declval<Args>()... ),		\
+					std::true_type());												\
+																					\
+	template<typename>																\
+	static constexpr std::false_type check(...);									\
+																					\
+public:																				\
+																					\
+	static constexpr bool value = decltype(check<T>(0))::value;						\
+}
 
 /*
  *
@@ -100,24 +94,21 @@ namespace mul																			\
  * 		cout << has_value_type_typedef<vector<int>>::value;
  */
 #define MUL_DECLARE_HAS_TYPEDEF_TRAITS(TYPE_TRAIT_NAME, HAS_TYPEDEF)	\
-namespace mul															\
-{																		\
-	template<class CLASS>												\
-	struct TYPE_TRAIT_NAME		 										\
-	{ 																	\
-	private:															\
-																		\
-		template<class C>												\
-		static char(&test(typename C::HAS_TYPEDEF *))[2];				\
-																		\
-		template<class>													\
-		static char(&test(...))[1];										\
-																		\
-	public:																\
-																		\
-		static constexpr bool value = sizeof(test<T>(0)) > 1; 			\
-	};																	\
-} // namespace mul
+template<class CLASS>												\
+struct TYPE_TRAIT_NAME		 										\
+{ 																	\
+private:															\
+																	\
+	template<class C>												\
+	static char(&test(typename C::HAS_TYPEDEF *))[2];				\
+																	\
+	template<class>													\
+	static char(&test(...))[1];										\
+																	\
+public:																\
+																	\
+	static constexpr bool value = sizeof(test<CLASS>(0)) > 1; 		\
+}
 
 
 
