b08b4015428dfdc07d27197b3f3fbb4860438d3e
Julien Combattelli <julien.combattelli@hotmail.com>
2018-04-25 22:59:07 +0200

    Remove mul::make_unique from MUL/Memory.h and add usefull byte swaping functions

--- source/MUL/Memory.hpp
+++ source/MUL/Memory.hpp
@@ -2,18 +2,18 @@
 // Name        : Memory.hpp
 // Author      : Julien Combattelli
 // EMail       : julien.combattelli@hotmail.com
-// Date		   : 22 jun. 2016
+// Date        : 22 jun. 2016
 // Version     : 1.0.0
 // Copyright   : This file is part of MUL project which is released under
 //               MIT license. See file LICENSE.txt for full license details
-// Description : It is an extension of the standard memory library
+// Description : Provide some tools to efficiently manipulate memory
 //============================================================================
 
-#ifndef MEMORY_HPP_
-#define MEMORY_HPP_
+#ifndef MUL_MEMORY_HPP_
+#define MUL_MEMORY_HPP_
 
 #if __cplusplus < 201103L
-  #error This library needs at least a C++11 compliant compiler
+#error This library needs at least a C++11 compliant compiler
 #endif
 
 #include <memory>
@@ -21,16 +21,33 @@
 namespace mul
 {
 
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args)
+inline std::uint32_t to_u32(const std::uint8_t bytes[4])
 {
-#if __cplusplus < 201402L
-	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-#else
-	return std::make_unique<T>(std::forward<Args>(args)...);
-#endif
+    return std::uint32_t(bytes[0]) << 24 | std::uint32_t(bytes[1]) << 16
+         | std::uint32_t(bytes[2]) <<  8 | std::uint32_t(bytes[3]);
+}
+
+inline std::uint64_t to_u64(const std::uint8_t bytes[8])
+{
+    uint32_t lbytes = to_u32(&bytes[0]);
+    uint32_t hbytes = to_u32(&bytes[4]);
+    return std::uint64_t(lbytes) << 32 | std::uint64_t(hbytes);
+}
+
+inline void to_bytes(std::uint32_t u32, std::uint8_t bytes[4])
+{
+    uint8_t* u8v = (uint8_t*)&u32;
+    uint32_t& bytes_as_u32 = *(uint32_t*)bytes;
+    bytes_as_u32 = to_u32(u8v);
+}
+
+inline void to_bytes(std::uint64_t u64, std::uint8_t bytes[8])
+{
+    uint8_t* u8v = (uint8_t*)&u64;
+    uint64_t& bytes_as_u64 = *(uint64_t*)bytes;
+    bytes_as_u64 = to_u64(u8v);
 }
 
 } // namespace mul
 
-#endif // MEMORY_HPP_
+#endif // MUL_MEMORY_HPP_
