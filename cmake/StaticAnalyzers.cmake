4887913bc310530c373b24134ee178acf5ddae27
Julien Combattelli <julien.combattelli@hotmail.com>
2020-03-05 13:45:41 +0100

    Add multiple cmake scripts based on cpp_starter_project

---
+++ cmake/StaticAnalyzers.cmake
@@ -0,0 +1,23 @@
+if(ENABLE_CPPCHECK)
+    find_program(CPPCHECK cppcheck)
+    if(CPPCHECK)
+        set(CMAKE_CXX_CPPCHECK 
+            ${CPPCHECK} 
+                --suppress=missingInclude 
+                --enable=all
+                --inconclusive
+        )
+    else()
+        message(SEND_ERROR "cppcheck requested but executable not found")
+    endif()
+endif()
+
+if(ENABLE_CLANG_TIDY)
+    find_program(CLANGTIDY clang-tidy)
+    if(CLANGTIDY)
+        set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY})
+    else()
+        message(SEND_ERROR "clang-tidy requested but executable not found")
+    endif()
+endif()
+
